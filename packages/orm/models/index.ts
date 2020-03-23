d2afc8445d8d225933f3c93ec987471eba78d4ed
Alex Strive <3083076-alexstrive@users.noreply.gitlab.com>
2020-03-23 21:15:40 +0300

    add index

---
+++ packages/orm/models/index.ts
@@ -0,0 +1,25 @@
+import ActiveSubstanceInMedicine from './ActiveSubstanceInMedicine'
+import Disease from './Disease'
+import DiseaseCase from './DiseaseCase'
+import DiseaseContradictions from './DiseaseContradictions'
+import Medicine from './Medicine'
+import Patient from './Patient'
+import PatientContradictions from './PatientContradictions'
+import Prescription from './Prescription'
+import Status from './Status'
+import Substance from './Substance'
+import SubstanceContradictions from './SubstanceContradictions'
+
+export default {
+  ActiveSubstanceInMedicine,
+  Disease,
+  DiseaseCase,
+  DiseaseContradictions,
+  Medicine,
+  Patient,
+  PatientContradictions,
+  Prescription,
+  Status,
+  Substance,
+  SubstanceContradictions
+}
